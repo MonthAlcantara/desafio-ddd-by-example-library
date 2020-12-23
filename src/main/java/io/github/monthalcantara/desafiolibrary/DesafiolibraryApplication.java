8b69dc248b75a1467daa4022e4aecd65bb88ae57
monthalcantara <montival_junior@yahoo.com.br>
2020-12-23 09:37:23 -0300

    Criacao do projeto

---
+++ src/main/java/io/github/monthalcantara/desafiolibrary/DesafiolibraryApplication.java
@@ -0,0 +1,13 @@
+package io.github.monthalcantara.desafiolibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiolibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafiolibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/monthalcantara/desafiolibrary/DesafiolibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.monthalcantara.desafiolibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiolibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
